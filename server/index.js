533bd35bef9f770961f4b079fc843a0b9328557d
AGOSTINHO <LaaGostim.OFICIAL@gmail.com>
2024-02-27 17:29:35 +0000

    v0.34 - Middleware
    Added middleware to handle errors

--- server/index.js
+++ server/index.js
@@ -22,3 +22,13 @@ app.listen(process.env.PORT || 5000, () => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode: statusCode,
+    message: message,
+  });
+});
