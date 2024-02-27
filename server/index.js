357d59eace28be6b6cbfc010b051ac6f18f0b3f0
AGOSTINHO <LaaGostim.OFICIAL@gmail.com>
2024-02-27 12:26:55 +0000

    v0.32 - Auth Requests
    Testing request on the API
    Integrated JSON into server

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import mongoose from "mongoose";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION)
@@ -11,10 +12,13 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
 const app = express();
+app.use(express.json());
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Server running...");
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
