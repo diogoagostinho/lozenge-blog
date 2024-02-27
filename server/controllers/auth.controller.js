e4f928e4d85f0f6a32e145180303fd1a98d5eae5
AGOSTINHO <LaaGostim.OFICIAL@gmail.com>
2024-02-27 14:20:29 +0000

    v0.33 - Password Hash
    Now POSTing users to DB, with encrypted password

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
 
 export const signup = async (req, res) => {
   const { username, email, password } = req.body;
@@ -13,9 +14,18 @@ export const signup = async (req, res) => {
   ) {
     return res.status(400).json({ message: "Please fill all required fields" });
   }
+
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+
   const newUser = new User({
     username: username,
     email: email,
-    password: password,
+    password: hashedPassword,
   });
+  try {
+    await newUser.save();
+    res.json({ message: `User ${username} sucessfully registered` });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
